f05ed63c4ef1282ce57560d52537bc490b3e57bf
Rufai-Ahmed <abbeyrufai234@gmail.com>
2025-03-24 17:01:34 +0100

    feat: testimonial

--- components/sections/joinWepay/testimonial-card.tsx
+++ components/sections/joinWepay/testimonial-card.tsx
@@ -13,13 +13,13 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
       className="relative w-full max-w-full sm:max-w-xl md:max-w-2xl lg:max-w-3xl mx-auto"
       style={{ background: "#e6fff0" }}
     >
-      <div className="relative flex items-center justify-center p-4 sm:p-6 md:p-8">
+      <div className="relative flex items-center justify-center p-4 px-0 sm:px-8 sm:p-6 md:p-8">
         <div className="absolute inset-0 size-full">
           <Image
             src="/images/border.svg"
             alt="Decorative border"
             fill
-            className="object-cie"
+            className="object-contain dark:bg-[#043024]" 
             priority
           />
         </div>
@@ -28,20 +28,20 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
           <h2 className="text-md sm:text-2xl font-semibold mb-2 sm:mb-4">
             Real Stories, Real Impact
           </h2>
-          <p className="text-gray-100 mb-4 sm:mb-6 md:mb-8 flex-grow text-xs sm:text-base">
+          <p className="text-gray-100 mb-4 sm:mb-6 md:mb-8 flex-grow text-xs sm:text-base line-clamp-1 md:line-clamp-6">
             {testimonial.content}
           </p>
 
           <div className="flex items-start sm:items-center justify-between mt-auto gap-4">
             <div>
-              <h3 className="font-semibold text-lg sm:text-xl">
+              <h3 className="font-semibold text-md sm:text-xl">
                 {testimonial.name}
               </h3>
               <p className="text-gray-300 text-xs sm:text-sm">
                 {testimonial.title}
               </p>
             </div>
-            <div className="w-12 h-12 sm:w-16 sm:h-16 rounded-full overflow-hidden border-2 border-green-300 self-end sm:self-auto">
+            <div className="w-10 h-10 sm:w-16 sm:h-16 rounded-full overflow-hidden border-2 border-green-300 self-end sm:self-auto">
               <img
                 src={
                   testimonial.avatar || "/placeholder.svg?height=64&width=64"
